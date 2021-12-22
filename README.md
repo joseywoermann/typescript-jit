97e03460c80f7f5bb44b28d32005d2bd83b08a45
Josey Wörmann <jcw05@gmx.de>
2021-12-22 23:57:40 +0100

    init

---
+++ README.md
@@ -0,0 +1,43 @@
+# typescript-jit
+
+## What is this?
+
+A JIT (just-in-time) compiler, aka. interpreter. It doesn't really have any features, I was just bored.
+
+## Should I use it?
+
+Absolutely not.
+
+## I want to use it anyway
+
+The following is an example program the prints `Hello!` to the console. Clone the repo, run `npm i` & `npm run ts:build`. Save the program in a file called `program.tsjit`and run`npm run start`.
+
+There are 256 available spaces in the "memory", which you can fill with [decimal numbers which correlate to ASCII characters](https://www.asciitable.com/). There are 7 Opcodes at the moment: `POS`, `GET`, `SET`, `DEBUG`, `CLEAR`, `PRINT`&`COPY`. These do different thing but I'm too lazy to document them.
+
+```
+POS:0
+SET:72
+PRINT
+
+POS:1
+SET:101
+PRINT
+
+POS:2
+SET:108
+PRINT
+
+POS:3
+SET:108
+PRINT
+
+POS:4
+SET:111
+PRINT
+
+POS:5
+SET:33
+PRINT
+```
+
+This project was inspired by [alii/alissembly](https://github.com/alii/alissembly).
---
+++ src/index.ts
@@ -0,0 +1,73 @@
+import fs from "fs";
+
+type Memory = number[];
+type Line = string;
+
+const resetMemory = (): void => {
+    for (let i = 0; i < 256; i++) {
+        memory[i] = 0;
+    }
+};
+
+const validLines = (raw: Line[]): Line[] => {
+    const valids: Line[] = [];
+
+    raw.forEach((line) => {
+        if (
+            line.startsWith("POS") ||
+            line.startsWith("SET") ||
+            line.startsWith("GET") ||
+            line.startsWith("DEBUG") ||
+            line.startsWith("CLEAR") ||
+            line.startsWith("PRINT") ||
+            line.startsWith("COPY")
+        ) {
+            valids.push(line.trimEnd());
+        }
+    });
+    return valids;
+};
+
+let memory: Memory = [];
+let currentAdress: number = 0;
+
+resetMemory();
+
+/**
+ * OP-CODES:
+ * ======================================================
+ * POS: Move pointer to given position
+ * SET: Set value to given number
+ * GET: Read value at current position
+ * DEBUG: Print entire memory
+ * CLEAR: Reset memory
+ * PRINT: Print code as character
+ * COPY: Copy value of given position to current position
+ */
+
+// Read program
+const rawLines: Line[] = fs.readFileSync("./program.tsjit").toString().split("\n");
+const lines: Line[] = validLines(rawLines);
+
+lines.forEach((line) => {
+    const [opcode, ...args] = line.split(":");
+
+    if (opcode === "CLEAR") {
+        resetMemory();
+    } else if (opcode === "DEBUG") {
+        console.log(memory);
+    } else if (opcode === "POS") {
+        currentAdress = Number(args[0]);
+    } else if (opcode === "SET") {
+        memory[currentAdress] = Number(args[0]);
+    } else if (opcode === "GET") {
+        console.log(memory[currentAdress]);
+    } else if (opcode === "PRINT") {
+        // console.log() automatically appends \n, we don't want that
+        process.stdout.write(String.fromCharCode(memory[currentAdress]));
+    } else if (opcode === "COPY") {
+        memory[currentAdress] = memory[Number(args[0])];
+    } else {
+        console.log("congratulations, you broken logic");
+    }
+});
